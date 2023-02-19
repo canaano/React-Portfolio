fb0c589789dc49b2b897707774e6acb3a3a2707d
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2023-02-19 10:15:57 -0800

    added card.jsx, now sorting multiples from the template i made

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Card from "./components/Card"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
@@ -9,7 +10,7 @@ function App() {
     <div>
       <Navbar />
       <Hero />
-
+      <Card />
     </div>
   )
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='grid grid-cols-1 bg-white rounded-xl max-w-xs'>
+        <img className='rounded-xl rounded-b-none' 
+        src='https://images.unsplash.com/photo-1676557059846-2dad064ae6e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'/>
+        <div className='p-4'>
+            <h2 className='font-bold'>Title</h2>
+            <p className='text-sm mt-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic architecto illum recusandae facilis perspiciatis! Facere repellendus odit consectetur ex doloribus. Sequi necessitatibus, a libero rem non recusandae veritatis explicabo. Eum.</p>
+            <button className='bg-blue-500 text-white px-2 py-1 rounded-full mt-3'>Repo</button>
+        </div>
+    </div>
+  )
+}
+
+export default Card
