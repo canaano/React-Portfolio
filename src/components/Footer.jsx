e1bed5c9a4cc28b1fd31caf7236fda8693c510bf
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2023-02-21 21:07:21 -0800

    added social icons in footer and linked to email, github, linkedIn

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { SiGithub, SiLinkedin, SiMinutemailer } from 'react-icons/si'
 
 const Footer = () => {
   return (
     <footer className='flex justify-center items-center solid border-t-2'>
         <ul className='text-blue-500 md:flex items-center'>
-            <li className='p-6'>Email</li>
-            <li className='p-6'>Github</li>
-            <li className='p-6'>LinkedIn</li>
+            <a href='mailto:canaan@simpledronemedia.com'>
+                <li className='p-6 text-3xl'><SiMinutemailer /></li>
+            </a>
+            <a href="https://github.com/canaano">
+                <li className='p-6 text-3xl'><SiGithub /></li>
+            </a>
+            <a href="https://www.linkedin.com/in/canaan-owens-5231581a3/">
+                <li className='p-6 text-3xl'><SiLinkedin /></li>
+            </a>
         </ul>
     </footer>
   )
