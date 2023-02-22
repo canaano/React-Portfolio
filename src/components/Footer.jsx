0eadcd6919bf166c44afd7e09c5c056f9c2a3371
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2023-02-21 18:40:16 -0800

    added footer formatting and prepping for updating with icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='w-full text-3xl font-bold m-4 text-blue-500 mx-auto text-center flex flex-col justify-center'>
-        <p>
-        Footer at the bottom
-        </p>
-        </footer>
+    <footer className='flex justify-center items-center solid border-t-2'>
+        <ul className='text-blue-500 md:flex items-center'>
+            <li className='p-6'>Email</li>
+            <li className='p-6'>Github</li>
+            <li className='p-6'>LinkedIn</li>
+        </ul>
+    </footer>
   )
 }
 
