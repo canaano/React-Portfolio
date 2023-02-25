18a10aa0b8705d4d13138f6751265e8bf51aa3d4
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2023-02-25 14:45:48 -0800

    added href to navbar buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,50 @@
-import React, {useState} from 'react'
-import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(false);
 
-    const handleNav = () => {
-        setNav(!nav)
-    }
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
   return (
-    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4  text-white'>
-        <h1 className='w-full text-3xl font-bold m-4 text-blue-500'>Portfolio</h1>
-        <ul className='hidden md:flex'>
-            <li className='p-5'>About</li>
-            <li className='p-5'>Projects</li>
-            <li className='p-5'>Contact</li>
-            <li className='p-5'>Resume</li>
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4  text-white">
+      <h1 className="w-full text-3xl font-bold m-4 text-blue-500">Portfolio</h1>
+      <ul className="hidden md:flex">
+        <a href="#about">
+          <li className="p-5">About</li>
+        </a>
+        <a href="#projects">
+          <li className="p-5">Projects</li>
+        </a>
+        <a href="#contact">
+          <li className="p-5">Contact</li>
+        </a>
+        <li className="p-5">Resume</li>
+      </ul>
+      <div onClick={handleNav} className="block md:hidden">
+        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+      <div
+        className={
+          !nav
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-500 bg-[#191919] ease-in-out duration-500"
+            : "fixed left-[-100%]"
+        }
+      >
+        <h1 className="w-full text-3xl font-bold text-blue-500 m-8">
+          Canaan Owens
+        </h1>
+        <ul className="uppercase p-4">
+          <li className="p-5 border-b border-gray-500">About</li>
+          <li className="p-5 border-b border-gray-500">Projects</li>
+          <li className="p-5 border-b border-gray-500">Contact</li>
+          <li className="p-5 border-b border-gray-500">Resume</li>
         </ul>
-        {/* <div onClick={handleNav} className='block md:hidden'>
-            {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
-        </div>
-        <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-500 bg-[#191919] ease-in-out duration-500' : 'fixed left-[-100%]'} >
-        <h1 className='w-full text-3xl font-bold text-blue-500 m-8'>Canaan Owens</h1>
-        <ul className='uppercase p-4'>
-            <li className='p-5 border-b border-gray-500'>About</li>
-            <li className='p-5 border-b border-gray-500'>Projects</li>
-            <li className='p-5 border-b border-gray-500'>Contact</li>
-            <li className='p-5 border-b border-gray-500'>Resume</li>
-        </ul>
-        </div> */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
